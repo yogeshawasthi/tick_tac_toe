e94983176a92deb85bd99824bc0d34a1e663783b
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-10-10 20:20:44 +0545

    New Project Tik Tak Toe

---
+++ src/Components/TickTacToe/TickTacToe.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle_icon from '../Assests/circle.png';
+import cross_icon from '../Assests/cross.png'
+
+const TickTackToe = () => {
+  return (
+    <div className='container'>
+      <h1 className='title'></h1>
+      
+    </div>
+
+  )
+}
+
+export default TickTackToe
